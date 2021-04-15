5a120cb1894f3acd3abe01652df5e13ed1ea09f5
Rajveer-Narang <rajveer2044cse18@chitkara.edu.in>
2021-04-15 14:53:19 +0530

    second commit

---
+++ home.js
@@ -0,0 +1,11 @@
+async function  sendata() {
+    let country = getElementById("country-name");
+    let sdate = getElementById("from");
+    // sdate contains starting date
+    let edate = getElementById("to");
+    // to contains ending date 
+
+    fetch()
+    
+
+}
